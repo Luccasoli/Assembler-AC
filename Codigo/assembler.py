d6614160e4527580ca119028077d67fc9a582df9
luccasoli <lucasoliveira98@outlook.com>
2017-12-14 21:33:17 -0300

    Dividi o código assembly em .data e .text

--- Codigo/assembler.py
+++ Codigo/assembler.py
@@ -15,11 +15,42 @@ while True:
 			# Acessa um arquivo para leitura
 			arq = open(file_name, 'r')
 
-			# Lê cada linha do arquivo e transforma em uma lista de strings
-			lista = arq.readlines()
+			#Lê cada linha do arquivo e transforma em uma lista de strings
+			codigoCompleto = arq.readlines()
+			#codigoCompleto.remove('')
 
-			for l in lista:
-				print(l[:-1])
+			# Lista de linhas de cada espaço
+			linhasData = []
+			linhasText = []
+			estouEmData = False
+			estouEmText = False
+
+			# Lê o código uma vez
+			for linha in codigoCompleto:
+				if(linha.find('.text') != -1):
+					estouEmText = True
+					estouEmData = False
+					
+				elif(estouEmText):
+					linhasText.append(linha)
+
+				elif(linha.find('.data') != -1):
+					estouEmText = False
+					estouEmData = True
+					
+				elif(estouEmData):
+					linhasData.append(linha)
+
+			print(".data:")
+			for aux in linhasData:
+				print(aux)
+			print(".text:")
+			for aux in linhasText:
+				print(aux)
+			#for l in lista:
+			#	print(l[:-1])
+			#print(codigoCompleto)
+			break
 
 		except FileNotFoundError as e:
 			print("O arquivo informado não existe!") 
@@ -29,4 +60,3 @@ while True:
 		print("O arquivo informado não é válido! (!= .asm)")
 		break
 
-	print("n imprime")
